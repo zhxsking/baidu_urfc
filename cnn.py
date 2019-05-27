72732bd2acaed41bd9f29655b4be4b103db6277a
zhxsking <1558560171@qq.com>
2019-05-27 22:10:05 +0800

    添加avgpool

--- cnn.py
+++ cnn.py
@@ -48,9 +48,8 @@ class CNN(nn.Module):
                 nn.ReLU(inplace=True),
                 nn.MaxPool2d(2),
                 )
-#        self.drop = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(128*3*3, 256)
-        self.fc2 = nn.Linear(256, 9)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(128, 9)
         
     def forward(self, x_img, x_visit):
         x_img = self.conv1(x_img)
@@ -58,124 +57,9 @@ class CNN(nn.Module):
         
         x = torch.cat((x_img, x_visit), dim=1)
         x = self.conv3(x)
-        x = x.view(x.size(0), -1)
         
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = F.log_softmax(x, dim=1)
-        return x
-    
-    
-class CNN11(nn.Module):
-    def __init__(self, in_depth, in_size, pretrained=True):
-        """VGG11迁移学习"""
-        super().__init__()
-
-        self.encoder = models.vgg11(pretrained=pretrained).features
-
-        self.relu = nn.ReLU(inplace=True)
-        
-        self.conv1 = nn.Sequential(nn.Conv2d(8, 64, 3, padding=1),
-                                   self.relu)
-
-        self.conv2 = nn.Sequential(self.encoder[3],
-                                   self.relu)
-
-        self.conv3 = nn.Sequential(
-            self.encoder[6],
-            self.relu,
-            self.encoder[8],
-            self.relu,
-        )
-        self.conv4 = nn.Sequential(
-            self.encoder[11],
-            self.relu,
-            self.encoder[13],
-            self.relu,
-        )
-
-        self.conv5 = nn.Sequential(
-            self.encoder[16],
-            self.relu,
-            self.encoder[18],
-            self.relu,
-        )
-        
-        self.fc = nn.Sequential(
-            nn.Linear(512*in_size*in_size, 4096),
-            self.relu,
-            nn.Linear(4096, 4),
-            self.relu,
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        x = F.log_softmax(x, dim=1)
-        return x
-    
-class CNN11_bn(nn.Module):
-    def __init__(self, in_depth, in_size, pretrained=True):
-        """VGG11迁移学习"""
-        super().__init__()
-
-        self.encoder = models.vgg11_bn(pretrained=pretrained).features
-
-        self.relu = nn.ReLU(inplace=True)
-        
-        self.conv1 = nn.Sequential(nn.Conv2d(8, 64, 3, padding=1),
-                                   self.encoder[1],
-                                   self.relu)
-
-        self.conv2 = nn.Sequential(self.encoder[4],
-                                   self.encoder[5],
-                                   self.relu)
-
-        self.conv3 = nn.Sequential(
-            self.encoder[8],
-            self.encoder[9],
-            self.relu,
-            self.encoder[11],
-            self.encoder[12],
-            self.relu,
-        )
-        self.conv4 = nn.Sequential(
-            self.encoder[15],
-            self.encoder[16],
-            self.relu,
-            self.encoder[18],
-            self.encoder[19],
-            self.relu,
-        )
-
-        self.conv5 = nn.Sequential(
-            self.encoder[22],
-            self.encoder[23],
-            self.relu,
-            self.encoder[25],
-            self.encoder[26],
-            self.relu,
-        )
-        
-        self.fc = nn.Sequential(
-            nn.Linear(512*in_size*in_size, 4096),
-            self.relu,
-            nn.Linear(4096, 4),
-            self.relu,
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = x.view(x.size(0), -1)
+        x = self.avgpool(x)
+        x = x.reshape(x.size(0), -1)
         x = self.fc(x)
         x = F.log_softmax(x, dim=1)
         return x
